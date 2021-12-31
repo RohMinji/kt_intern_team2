4e898f1d113b55c89c8de5138f0e9710ddbca8e9
sbsbpark <seokbin9653@gmail.com>
2021-12-31 10:28:26 +0900

    Update views.py
    된건가..

--- core/views.py
+++ core/views.py
@@ -4,22 +4,27 @@ from django.http import StreamingHttpResponse
 import cv2
 import threading
 import dlib
+import numpy as np
 import imutils
 from imutils import face_utils
 from scipy.spatial import distance as dist
+from imutils import face_utils
+from scipy.spatial import distance as dist
+import numpy as np
 
-
-FACIAL_LANDMARK_PREDICTOR = "shape_predictor_68_face_landmarks.dat"  
+ 
+predictor = "shape_predictor_68_face_landmarks.dat"  
 MINIMUM_EAR = 0.2
-MAXIMUM_FRAME_COUNT = 10
+MAXIMUM_FRAME_COUNT = 5
 EYE_CLOSED_COUNTER = 0
 BLINK_COUNT = 0
 YAWN_COUNTER = 0
 yawn_status = False 
 
 
-faceDetector = dlib.get_frontal_face_detector() # 얼굴인식
-landmarkFinder = dlib.shape_predictor(FACIAL_LANDMARK_PREDICTOR)
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat') #랜드마크 추출
+detector = dlib.get_frontal_face_detector() # 얼굴인식
+#landmarkFinder = dlib.shape_predictor(predictor)
 
 (leftEyeStart, leftEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rightEyeStart, rightEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
@@ -34,38 +39,52 @@ class VideoCamera(object):
 
     def add_overlays(self, image):
         # image = imutils.resize(image, width=400)
+        global yawn_status
+        global grayImage
+        global BLINK_COUNT
+        global YAWN_COUNTER
+        global EYE_CLOSED_COUNTER
+
+        image_landmarks, lip_distance = mouth_open(image)
         grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceDetector(grayImage, 0)
+        faces = detector(grayImage, 0)
+
+        if len(faces)<1:
+            cv2.putText(image, "No Student", (50,450),
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255),2)
 
         for face in faces:
-            faceLandmarks = landmarkFinder(grayImage, face)
-            faceLandmarks = face_utils.shape_to_np(faceLandmarks)
+            ear= calEAR(face)
+            if ear < MINIMUM_EAR:
+                EYE_CLOSED_COUNTER += 1
+            else:
+                EYE_CLOSED_COUNTER = 0
+            #cv2.putText(frame, "EAR: {}".format(round(ear, 1)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-            leftEye = faceLandmarks[leftEyeStart:leftEyeEnd]
-            rightEye = faceLandmarks[rightEyeStart:rightEyeEnd]
+            if EYE_CLOSED_COUNTER >= MAXIMUM_FRAME_COUNT:
+                cv2.putText(image, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                BLINK_COUNT += 1
+                cv2.putText(image, "Count: {}".format(int((BLINK_COUNT)/5)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+        prev_yawn_status = yawn_status
+        if lip_distance>25:
+            yawn_status = True 
+        #cv2.putText(frame, "Subject is Yawning", (50,450), 
+        #           cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255),2)
+        
+            output_text = " Yawn Count: " + str(YAWN_COUNTER + 1)
 
-            leftEAR = eye_aspect_ratio(leftEye)
-            rightEAR = eye_aspect_ratio(rightEye)
-
-            ear = (leftEAR + rightEAR) / 2.0
-
-            leftEyeHull = cv2.convexHull(leftEye)
-            rightEyeHull = cv2.convexHull(rightEye)
-
-            cv2.drawContours(image, [leftEyeHull], -1, (255, 0, 0), 2)
-            cv2.drawContours(image, [rightEyeHull], -1, (255, 0, 0), 2)
-
-            # if ear < MINIMUM_EAR:
-            #     EYE_CLOSED_COUNTER += 1
-            # else:
-            #     EYE_CLOSED_COUNTER = 0
-
-            cv2.putText(image, "EAR: {}".format(round(ear, 1)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-            # if EYE_CLOSED_COUNTER >= MAXIMUM_FRAME_COUNT:
-            cv2.putText(image, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) # 졸음 탐지 이벤트 발생
+            cv2.putText(image, output_text, (50,50),
+                    cv2.FONT_HERSHEY_COMPLEX, 1,(0,255,127),2)
+        else:
+            yawn_status = False 
+         
+        if prev_yawn_status == True and yawn_status == False:
+            YAWN_COUNTER += 1
+    
 
     def get_frame(self):
+        global image
         image = self.frame
         self.add_overlays(image)
         # jpeg encoding
@@ -84,6 +103,15 @@ def gen(camera):
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+def get_landmarks(im):
+    rects = detector(im, 1)
+
+    if len(rects) > 1:
+        return "error"
+    if len(rects) == 0:
+        return "error"
+    return np.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])
+
 
 def eye_aspect_ratio(eye):
     
@@ -91,6 +119,76 @@ def eye_aspect_ratio(eye):
     p3_minus_p5 = dist.euclidean(eye[2], eye[4])
     p1_minus_p4 = dist.euclidean(eye[0], eye[3])
     ear = (p2_minus_p6 + p3_minus_p5) / (2.0 * p1_minus_p4)
+    return ear
+
+
+def annotate_landmarks(im, landmarks):
+    im = im.copy()
+    for idx, point in enumerate(landmarks):
+        pos = (point[0, 0], point[0, 1])
+        cv2.putText(im, str(idx), pos,
+                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+                    fontScale=0.4,
+                    color=(0, 0, 255))
+        cv2.circle(im, pos, 3, color=(0, 255, 255))
+    return im
+
+def top_lip(landmarks):
+    top_lip_pts = []
+    for i in range(50,53):
+        top_lip_pts.append(landmarks[i])
+    for i in range(61,64):
+        top_lip_pts.append(landmarks[i])
+    top_lip_all_pts = np.squeeze(np.asarray(top_lip_pts))
+    top_lip_mean = np.mean(top_lip_pts, axis=0)
+    return int(top_lip_mean[:,1])
+
+def bottom_lip(landmarks):
+    bottom_lip_pts = []
+    for i in range(65,68):
+        bottom_lip_pts.append(landmarks[i])
+    for i in range(56,59):
+        bottom_lip_pts.append(landmarks[i])
+    bottom_lip_all_pts = np.squeeze(np.asarray(bottom_lip_pts))
+    bottom_lip_mean = np.mean(bottom_lip_pts, axis=0)
+    return int(bottom_lip_mean[:,1])
+
+def mouth_open(imagee):
+    landmarks = get_landmarks(imagee)
+    
+    if landmarks == "error":
+        return imagee, 0
+    
+    image_with_landmarks = annotate_landmarks(imagee, landmarks)
+    top_lip_center = top_lip(landmarks)
+    bottom_lip_center = bottom_lip(landmarks)
+    lip_distance = abs(top_lip_center - bottom_lip_center)
+    return image_with_landmarks, lip_distance
+
+    #cv2.imshow('Result', image_with_landmarks)
+    #cv2.imwrite('image_with_landmarks.jpg',image_with_landmarks)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+def calEAR(face):
+    
+    faceLandmarks = predictor(grayImage, face)
+    faceLandmarks = face_utils.shape_to_np(faceLandmarks)
+
+    leftEye = faceLandmarks[leftEyeStart:leftEyeEnd]
+    rightEye = faceLandmarks[rightEyeStart:rightEyeEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+
+    cv2.drawContours(image, [leftEyeHull], -1, (255, 0, 0), 2)
+    cv2.drawContours(image, [rightEyeHull], -1, (255, 0, 0), 2)
+    
     return ear
 
 # while True:
