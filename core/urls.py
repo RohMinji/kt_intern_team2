084c4186e08dc314015f70d30b1413bd031e4d8d
InjiChoi <chldlswl1212@gmail.com>
2022-01-02 16:05:34 +0900

    feat: SY detection filtering in main page

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from models import face_detection
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="main"),
+    path("sy-detection/", face_detection.sy_detection, name="sy_detection"),
     path("cam/", views.cam_test, name="cam_test"),
     path("face-detection/", views.face_detection, name="face_detection"),
     path("pose-detection/", views.pose_detection, name="pose_detection"),
