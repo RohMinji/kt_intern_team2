31bffa6a2cea02e0ce9b90559b3b8bcc36868705
InjiChoi <chldlswl1212@gmail.com>
2022-01-04 13:38:14 +0900

    fix: change course video

--- recognitions/views.py
+++ recognitions/views.py
@@ -8,14 +8,15 @@ def course(request):
 def result(request):
     from models.sleep_detection import YAWN_COUNTER
     from models.dance_detection import avg_score
-
-    print('-------------------------', avg_score)
-
+    try:
+        avg_score = int(avg_score)
+    except:
+        avg_score = "No Score"
     context = {
         "key": list(eyesize_dic.keys()),
         "val": list(eyesize_dic.values()),
         "cnt_empty":list(empty_dic.values()).count(1),
         "YAWN_COUNTER": YAWN_COUNTER,
-        "avg_score": int(avg_score),
+        "avg_score": avg_score,
     }
     return render(request, "recognitions/result.html", context)
