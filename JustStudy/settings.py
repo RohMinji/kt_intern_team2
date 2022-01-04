b57898fbe23062fdab58983bb97d0049df278722
InjiChoi <chldlswl1212@gmail.com>
2022-01-04 10:39:18 +0900

    fix: delete rest framework

--- JustStudy/settings.py
+++ JustStudy/settings.py
@@ -139,5 +139,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
