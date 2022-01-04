eca84e6faaac15447d718dea6425af433d14362e
hbgittt <hbjoo318@gmail.com>
2022-01-04 15:46:36 +0900

    fix

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("video-stop/",  sleep_detection.videoStop, name="videoStop"),
     path("cam/", views.cam_test, name="cam_test"),
     path("face-detection/", views.face_detection, name="face_detection"),
+    path("pose-detection/", views.pose_detection, name="pose_detection"),
 ]
--- models/face_detection.py
+++ models/face_detection.py
@@ -1,21 +1,25 @@
 import cv2
 import numpy as np
+import socket
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
-from django.http import JsonResponse
 
-# Loading User's model
+from django.http import StreamingHttpResponse, JsonResponse
+
+
 model = load_model('models/keras_model.h5', compile=False)
+
+
 model.summary()
 
-# Counting the number of frames to identify user
+
 SY_COUNT = 0
 
-# Face Detection
 def face_detect(cam, image):
     global SY_COUNT
+
     webcam = cam
     web_frame = image
 
@@ -23,19 +27,27 @@ def face_detect(cam, image):
         print("Could not open webcam")
         exit()
     
-    # Loop through Frames
+    # loop through frames
     img = cv2.resize(web_frame, (224, 224), interpolation = cv2.INTER_AREA)
     x = img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
     prediction = model.predict(x)
-    predicted_class = np.argmax(prediction[0]) # predicted class 0, 1, 2
+    predicted_class = np.argmax(prediction[0]) # 예측된 클래스 0, 1, 2
 
     if predicted_class == 0:
+        msg = "안녕하세요 승용님, 학습을 시작하겠습니다."
         SY_COUNT += 1
+        
+    elif predicted_class == 1:
+        msg = "교육생이 아닙니다."   
+        
+    elif predicted_class == 2:
+        msg = ""
 
-# Deliver Data
+    ##print(predicted_class)
+
 def sy_detection(request):
     global SY_COUNT
     data = {
